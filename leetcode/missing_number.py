edd9c46c52855e0249cadd8e35fd1e72e86b6dc8
Esteban Raigoza <91690458+EstCodes@users.noreply.github.com>
2026-01-12 23:01:20 +0000

    modified:   leetcode/missing_number.py

--- leetcode/missing_number.py
+++ leetcode/missing_number.py
@@ -1,16 +1,18 @@
 """
 You are given all numbers between 1,2,...,n except one. Your task is to find the missing number.
 """
+import random
 
 def missing_nummber():
 
-    origlist = [1, 2, 3, 5] # Modify for testing
+    origlist = []
     listn = []
 
     while True:
         n = int(input("Input a number: "))
         if n == int or n > 0:
             listn.append(n)
+            origlist.append(n)
             break
         else:
             print("Enter a valid positive number.")
@@ -18,10 +20,24 @@ def missing_nummber():
     while n > 1:
         n -= 1
         listn.append(n)
+        origlist.append(n)
 
-    print(f"This is the saved list: {listn}")
-    print(f"This is the original list: {origlist}")
-    print(f"Missing value: {set(listn).difference(set(origlist))}")
+    length_list = len(listn)
+    print(f"How many elements does the list has? : {length_list}")
+    rand_number = random.randrange(0, length_list)
+
+    listn.pop(rand_number)
+
+    origlist = set(origlist)
+    listn = set(listn)
+
+    missing_number_set = origlist - listn
+    missing_number = sorted(list(missing_number_set))
+
+    print(f"Original List: {origlist}")
+    print(f"Modified List: {listn}")
+    
+    print(f"Missing value: {missing_number}")
 
 
 missing_nummber()
