e027b4e6517dae6eae55a6a469c9bc42c7b16b01
Esteban Raigoza <91690458+EstCodes@users.noreply.github.com>
2026-01-18 21:13:27 +0000

    modified:   leetcode/increasing_array.py

--- leetcode/increasing_array.py
+++ leetcode/increasing_array.py
@@ -6,8 +6,8 @@ You are given an array of n integers. You want to modify the array so that it is
 On each move, you may increase the value of any element by one. What is the minimum number of moves required?
 """
 
-def IncreasingArray(Arr = None, random_amount = int, rand_in_num=int, current_i=int, next_i=int, diff=int, list_diff=None, total_moves=int):
-    if Arr is None and list_diff is None:
+def IncreasingArray(Arr=None, list_diff=None, actual_torrent=None):
+    if Arr is None and list_diff is None and actual_torrent is None:
         Arr = []
         list_diff = []
         try:
@@ -22,10 +22,21 @@ def IncreasingArray(Arr = None, random_amount = int, rand_in_num=int, current_i=
             current_i = Arr[i]
             next_i = Arr[i+1]
 
-            diff = current_i - next_i
-            list_diff.append(diff)
+            if next_i < current_i:
+                diff = current_i - next_i
+                list_diff.append(diff)
+                
+                if actual_torrent is None: 
+                    actual_torrent = current_i
+            elif actual_torrent is not None and actual_torrent < next_i:
+                actual_torrent = next_i
 
+            else:
+                continue
+        
         total_moves = sum(list_diff)
-        print(total_moves)
+        print(f"Total moves: {total_moves}")
+
+        
 
 IncreasingArray()
